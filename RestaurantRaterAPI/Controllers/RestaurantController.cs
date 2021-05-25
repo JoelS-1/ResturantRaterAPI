57445d0d1fb16200380c7391029529ac0395c514
Joel Stults <stultsjoel@gmail.com>
2021-05-25 13:13:49 -0400

    added restaurant class and controller. added migrations

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,34 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if(model == null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+
+            if (ModelState.IsValid)
+            {
+                //adding model to db context
+                _context.Restaurants.Add(model);
+                //then updating the database with the new model included -- await will wait until the task is done before continuing
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ RestaurantRaterAPI/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace RestaurantRaterAPI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RestaurantRaterAPI.Models.RestaurantDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "RestaurantRaterAPI.Models.RestaurantDbContext";
+        }
+
+        protected override void Seed(RestaurantRaterAPI.Models.RestaurantDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
